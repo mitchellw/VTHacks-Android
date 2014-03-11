7b4305ab18d54fcda93642c7ecbac2a192426c2d
ObeliskIsHuge <ObeliskIsHuge@gmail.com>
2014-03-11 09:14:38 -0400

    saving before messing with facebook

--- src/com/vt/vthacks/ScheduleActivity.java
+++ src/com/vt/vthacks/ScheduleActivity.java
@@ -1,31 +1,29 @@
 package com.vt.vthacks;
 
-
 import android.os.Bundle;
 import android.app.Activity;
 
 // -------------------------------------------------------------------------
 /**
  *
- *  This class handles the interaction for the announcements page
+ *  This class handles the interaction/data for the Schedule page
  *
  *  @author Brandon Potts
  *  @version Mar 10, 2014
  */
-public class AnnouncementsActivity
+public class ScheduleActivity
     extends Activity
 {
 
-
     // ----------------------------------------------------------
     /**
-     * Sets up the announcements page
+     * Sets up the Schedule page
      *
      * @param savedInstanceState is data that was most recently supplied
      */
     protected void OnCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.announcements);
+        setContentView(R.layout.schedule);
     }
 }
