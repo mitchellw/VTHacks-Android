b2d6eeca66ee7644c46b4bff6cc637fe6e4f0771
Wilson Mitchell <wilsonmitchell92@gmail.com>
2014-04-09 16:30:51 -0400

    Added splash screen.

--- src/com/vt/vthacks/SplashScreenActivity.java
+++ src/com/vt/vthacks/SplashScreenActivity.java
@@ -2,6 +2,7 @@ package com.vt.vthacks;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 
 // -------------------------------------------------------------------------
 /**
@@ -11,21 +12,33 @@ import android.app.Activity;
  * @version Mar 10, 2014
  */
 public class SplashScreenActivity
-    extends Activity
+extends Activity
 {
 
-    // ----------------------------------------------------------
-    /**
-     * Sets up the Schedule page
-     *
-     * @param savedInstanceState
-     *            is data that was most recently supplied
-     */
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.schedule);
+	// ----------------------------------------------------------
+	/**
+	 * Sets up the Schedule page
+	 *
+	 * @param savedInstanceState
+	 *            is data that was most recently supplied
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_screen);
 
-    }
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				startActivity(new Intent(SplashScreenActivity.this, MainMenuActivity.class));
+			}
+		}).start();
+	}
 }
